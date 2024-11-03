fa1ea29e430640bd9b3412e0450aaa3eeb6a414e
Brad <bsoverns@gmail.com>
2024-11-03 10:48:38 -0500

    Fixed a stored procedure

--- TwitchData/dbo/StoredProcedures/UpsertUser.sql
+++ TwitchData/dbo/StoredProcedures/UpsertUser.sql
@@ -1,58 +1,60 @@
 ﻿CREATE PROCEDURE [dbo].[UpsertUser]
 (
-	@TwitchUserId VARCHAR(50) = NULL,
-	@UserName VARCHAR(50),
-	@InteractionDateUtc DATETIME NULL,
-	@UserId INT NULL OUTPUT
+    @TwitchUserId VARCHAR(50) = NULL,
+    @UserName VARCHAR(50),
+    @InteractionDateUtc DATETIME NULL,
+    @UserId INT NULL OUTPUT
 )
 AS
 BEGIN
-	BEGIN TRY
-	BEGIN TRANSACTION
-		IF (@UserName IS NULL)
-		BEGIN
-			EXEC [dbo].[InsertErrorTrackInfo] 'UpsertUser', 'UserName cannot be NULL.';				
-			RETURN;
-		END
-				
-		IF (@InteractionDateUtc IS NULL)
-			SET @InteractionDateUtc = GETUTCDATE();
+    BEGIN TRY
+        BEGIN TRANSACTION
+            IF (@UserName IS NULL)
+            BEGIN
+                EXEC [dbo].[InsertErrorTrackInfo] 'UpsertUser', 'UserName cannot be NULL.';                
+                RETURN;
+            END
+                    
+            IF (@InteractionDateUtc IS NULL)
+                SET @InteractionDateUtc = GETUTCDATE();
 
-		MERGE INTO [dbo].[Users] AS t
-		USING 
-		(
-			SELECT @TwitchUserId AS TwitchUserId, @UserName AS UserName, @InteractionDateUtc AS InteractionDateUtc
-		) AS s
-		ON t.UserName = s.UserName
-		WHEN MATCHED AND
-		(
-			ISNULL(t.TwitchUserId, '') <> ISNULL(t.TwitchUserId, '') OR 
-			ISNULL(t.LastInteractionDateTimeUtc, '1900-01-01') != ISNULL(s.InteractionDateUtc, '1900-01-01')
-		)	
-		THEN
-			UPDATE SET 
-				t.LastInteractionDateTimeUtc = @InteractionDateUtc,
-				t.TwitchUserId = ISNULL(t.TwitchUserId, s.TwitchUserId)
-		WHEN NOT MATCHED THEN
-			INSERT (TwitchUserId, UserName, FirstInteractionDateTimeUtc, LastInteractionDateTimeUtc)
-			VALUES (s.TwitchUserId, s.UserName, @InteractionDateUtc, @InteractionDateUtc);
+            DECLARE @UserTable TABLE (UserId INT);
 
-		IF (@@ROWCOUNT > 0)
-			SELECT @UserId = SCOPE_IDENTITY();
+            MERGE INTO [dbo].[Users] AS t
+            USING 
+            (
+                SELECT @TwitchUserId AS TwitchUserId, @UserName AS UserName, @InteractionDateUtc AS InteractionDateUtc
+            ) AS s
+            ON t.UserName = s.UserName
+            WHEN MATCHED AND
+            (
+                ISNULL(t.TwitchUserId, '') <> ISNULL(t.TwitchUserId, '') OR 
+                ISNULL(t.LastInteractionDateTimeUtc, '1900-01-01') != ISNULL(s.InteractionDateUtc, '1900-01-01')
+            )   
+            THEN
+                UPDATE SET 
+                    t.LastInteractionDateTimeUtc = @InteractionDateUtc,
+                    t.TwitchUserId = ISNULL(t.TwitchUserId, s.TwitchUserId)
+            WHEN NOT MATCHED THEN
+                INSERT (TwitchUserId, UserName, FirstInteractionDateTimeUtc, LastInteractionDateTimeUtc)
+                VALUES (s.TwitchUserId, s.UserName, @InteractionDateUtc, @InteractionDateUtc)
+            OUTPUT inserted.UserId INTO @UserTable;
 
-		IF (@UserId IS NULL)
-			SELECT @UserId = UserId FROM [dbo].[Users] WHERE UserName = @UserName;
+            SELECT @UserId = UserId FROM @UserTable;
 
-		SELECT @UserId;
-		COMMIT TRANSACTION
-	END TRY
-	BEGIN CATCH
-	ROLLBACK TRANSACTION;
-		DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
-		DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
-		DECLARE @ErrorState INT = ERROR_STATE();
-		RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
-		EXEC [dbo].[InsertErrorTrackInfo] 'UpsertUser', @ErrorMessage;
-	END CATCH
-END
+            IF (@UserId IS NULL)
+                SELECT @UserId = UserId FROM [dbo].[Users] WHERE UserName = @UserName;
+
+            SELECT @UserId;
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+        DECLARE @ErrorState INT = ERROR_STATE();
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+        EXEC [dbo].[InsertErrorTrackInfo] 'UpsertUser', @ErrorMessage;
+    END CATCH
+END;
 GO
