bf02a4b2fdfa7ff790f62f2d348da89a9bed0dca
Brad <bsoverns@gmail.com>
2024-11-11 23:28:33 -0500

    Created views and started working on the UI

---
+++ TwitchTools/Classes/EncryptionClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TwitchTools
+{
+    class EncryptionClass
+    {
+        private static string Key = "Jeina;3in4#jhfdasioc8Nn3nolahpa%"; //32 CHARACTERS
+        private static string IV = "je4n4mc*sal;JNik"; //16 CHARACTERS
+        private static string return_data = "";
+
+        public static string Encrypt(string text)
+        {
+            try
+            {
+                byte[] plaintextbytes = System.Text.ASCIIEncoding.ASCII.GetBytes(text);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.BlockSize = 128;
+                    aes.KeySize = 256;
+                    aes.Key = System.Text.ASCIIEncoding.ASCII.GetBytes(Key);
+                    aes.IV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV);
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.Mode = CipherMode.CBC;
+                    ICryptoTransform crypto = aes.CreateEncryptor(aes.Key, aes.IV);
+                    byte[] encrypted = crypto.TransformFinalBlock(plaintextbytes, 0, plaintextbytes.Length);
+                    crypto.Dispose();
+
+                    return_data = Convert.ToBase64String(encrypted);
+                }
+
+                return return_data;
+            }
+
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());                
+                return return_data;
+            }
+
+            finally
+            {
+                return_data = "";
+            }
+        }
+
+        public static string Decrypt(string encrypted)
+        {
+            try
+            {
+                byte[] encryptedbytes = Convert.FromBase64String(encrypted);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.BlockSize = 128;
+                    aes.KeySize = 256;
+                    aes.Key = System.Text.ASCIIEncoding.ASCII.GetBytes(Key);
+                    aes.IV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV);
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.Mode = CipherMode.CBC;
+                    ICryptoTransform crypto = aes.CreateDecryptor(aes.Key, aes.IV);
+                    byte[] secret = crypto.TransformFinalBlock(encryptedbytes, 0, encryptedbytes.Length);
+                    crypto.Dispose();
+
+                    return_data = System.Text.ASCIIEncoding.ASCII.GetString(secret);
+                }
+
+                return return_data;
+            }
+
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());                
+                return return_data;
+            }
+
+            finally
+            {
+                return_data = "";
+            }
+        }
+    }
+}
---
+++ TwitchTools/Classes/SQLProcess.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Data.SqlClient;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TwitchTools
+{
+    class SQLProcess
+    {
+        #region SQLCode
+
+        #region SQLQueries
+
+        string _GetAccountsInfo = @"dbo.GetAccounts";
+
+        #endregion SQLQueries
+
+        #region SQLInsert 
+        string _InsertApiCount = @"dbo.InsertApiCallCount";
+
+        #endregion SQLInsert
+
+        #region SQLUpdates
+
+        string _UpdateAccessAuthorizations = @"UPDATE dbo.Authorizations
+SET AccessToken = @AccessToken,
+	AccessTokenExpiresIn = @AccessExpire,
+    AccessTokenExpireTimeUtc = DATEADD(SECOND, @AccessExpire, GETUTCDATE()),
+	RedirectUrl = 'http://localhost'
+WHERE AuthorizationId = 2";      
+
+        #endregion SQLUpdates
+
+        #region SQLUpsert
+
+        string _UpsertAccounts = @"dbo.UpsertAccounts";
+
+        #endregion SQLUpsert      
+
+        #region SQLDelete
+
+        string _DeleteOldApiCallLogs = @"[dbo].[DeleteOldApiCount]";
+
+        #endregion SQLDelete
+
+        #endregion SQLCode
+
+        public DataTable GetActiveStockList(SQLConnectionClass SQLConnect)
+        {
+            DataTable returnTable = new DataTable();
+            returnTable.Clear();
+
+            try
+            {
+                if (SQLConnect.UseIPAdress == true)
+                {
+                    IPAddress ip = new IPHelper().get_ip_from_host_name(SQLConnect.DataSource);
+
+                    using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection("Data Source=" + SQLConnect.DataSource + ";Initial Catalog=" + SQLConnect.InitialCatalog + ";User ID=" + SQLConnect.UserID + ";Password=" + SQLConnect.Password + ";Connect Timeout = 60;"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand(_GetAccountsInfo, con))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            using (SqlDataAdapter readActiveStocks = new SqlDataAdapter(cmd))
+                            {
+                                readActiveStocks.Fill(returnTable);
+
+                                //using (DataTable tempStorageTable = new DataTable())
+                                //{
+                                //    readActiveStocks.Fill(tempStorageTable);
+                                //    returnTable = tempStorageTable;
+                                //}
+                            }
+                        }
+                    }
+                }
+
+                else
+                {
+                    using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection("Data Source=" + SQLConnect.DataSource + ";Initial Catalog=" + SQLConnect.InitialCatalog + ";User ID=" + SQLConnect.UserID + ";Password=" + SQLConnect.Password + ";Connect Timeout = 60;"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand(_GetAccountsInfo, con))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            using (SqlDataAdapter readActiveStocks = new SqlDataAdapter(cmd))
+                            {
+                                readActiveStocks.Fill(returnTable);
+                            }
+                        }
+                    }
+                }
+
+                if (returnTable.Rows.Count == 0)
+                {
+                    SqlConnection sqlConnString = new SqlConnection("Data Source=" + SQLConnect.DataSource + ";Initial Catalog=" + SQLConnect.InitialCatalog + ";User ID=" + SQLConnect.UserID + ";Password=" + SQLConnect.Password + ";Connect Timeout = 60;");
+
+                    SqlDataAdapter readStuff = new SqlDataAdapter(_GetAccountsInfo, sqlConnString);
+
+                    sqlConnString.Open();
+                    readStuff.Fill(returnTable);
+                    sqlConnString.Close();
+                }
+
+                return returnTable;
+            }
+
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                return returnTable;
+            }
+
+            finally
+            {
+                returnTable.Dispose();
+            }
+        }       
+
+        public string cleanCode(string item)
+        {
+            Regex digitsOnly = new Regex(@"[^\d]");
+            return digitsOnly.Replace(item, "");
+        }
+
+        public string cleanBody(string body)
+        {
+            string newBody = body.Replace("\r\n", "\r");
+            newBody = newBody.Replace("\n", "\r");
+            newBody = newBody.Replace("\r", "\r\n");
+            return newBody;
+        }
+    }
+}
