54b59fdb89746febceef8aa18dca14f5345d4d27
Brad <bsoverns@gmail.com>
2024-11-17 11:05:55 -0500

    Fixed some UI elements and started incorperating ModelView changes

---
+++ TwitchTools/Classes/TwitchUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchTools
+{
+    internal class TwitchUser
+    {
+        public string UserName { get; set; }
+        //public string DisplayName { get; set; }
+        //public string UserId { get; set; }
+        //public string UserColor { get; set; }
+        //public string UserType { get; set; }
+        //public string UserStatus { get; set; }
+        //public string UserGame { get; set; }
+        //public string UserTitle { get; set; }
+        //public string UserFollowers { get; set; }
+        //public string UserViews { get; set; }
+        //public string UserPartner { get; set; }
+        //public string UserSubscribers { get; set; }
+        //public string UserSubPoints { get; set; }
+        //public string UserSubTier { get; set; }
+        //public string UserSubPlan { get; set; }
+        //public string UserSubStatus { get; set; }
+        //public string UserSubMessage { get; set; }
+        //public string UserSubMonths { get; set; }
+        //public string UserSubMonthsStreak { get; set; }
+        //public string UserSubMonthsTotal { get; set; }
+        //public string UserSubMonthsPrime { get; set; }
+        //public string UserSubMonthsGifted { get; set; }
+        //public string UserSubMonthsGiftedTotal { get; set; }
+        //public string UserSubMonthsGiftedPrime { get; set; }
+        //public string UserSubMonthsGiftedTier { get; set; }
+        //public string UserSubMonthsGiftedPlan { get; set; }
+        //public string UserSubMonthsGiftedStatus { get; set; }
+        //public string UserSubMonthsGiftedMessage { get; set; }
+        //public string UserSubMonthsGiftedFrom { get; set; }
+        //public string UserSubMonthsGiftedTo { get; set; }
+        //public string UserSubMonthsGiftedGifts { get; set; }
+        //public string UserSubMonthsGiftedGiftsTotal { get; set; }
+        //public string UserSubMonthsGiftedGiftsPrime { get; set; }
+        //public string UserSubMonthsGiftedGiftsTier { get; set; }
+        //public string UserSubMonthsGiftedGiftsPlan { get; set; }
+        //public string UserSubMonthsGiftedGiftsStatus { get; set; }
+        //public string UserSubMonthsGiftedGiftsMessage { get; set; }
+        //public string UserSubMonthsGiftedGiftsFrom { get; set; }
+    }
+}
---
+++ TwitchTools/Classes/TwitchUserChat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchTools
+{
+    internal class TwitchUserChat
+    {
+        public string UserName { get; set; }
+        public string ChatMessage { get; set; }
+
+        public string ChannelName { get; set; }
+    }
+}
